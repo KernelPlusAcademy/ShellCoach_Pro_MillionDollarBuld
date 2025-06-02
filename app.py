9274df9ebef9ab04bb71eb878c5ed5f80266ac01
KernelPlusAcademy <KernelPlusAcademy@gmail.com>
2025-06-01 23:27:04 -0400

    app.py

--- app.py
+++ app.py
@@ -75,4 +75,5 @@ def dashboard():
     return render_template('dashboard.html', explanation=explanation)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
+
